ca77745501199d9bab546e9c115685ac3804f09f
alex2231 <106402311+alex2231@users.noreply.github.com>
2022-12-26 18:33:09 -0500

    Importing React into VS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React JS
+          <br />
+          <Weather />
         </a>
       </header>
     </div>
